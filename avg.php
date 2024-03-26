9ccc9f5ca32277861b65336f1993913611a65c32
JulianTrel <144231197+JulianTrel@users.noreply.github.com>
2024-03-26 13:20:27 +0100

    commit

--- avg.php
+++ avg.php
@@ -8,7 +8,11 @@ include("header.html");
     <p>Telefoonnummer</p>
     <p>E-mailadres</p> 
     </article><br>
+    <h2>Uw gegevens</h2>
     <article>Deze gegevens zullen alleen gebruikt worden met als doel van reservering bij Hot Desi Handi</article>
     <article>De ontvanger van deze gegevens zal een werknemer zijn die uw persoongegevens zal gebruiken voor het inplannen van uw reservering</article>
-    <article>Uw gegevens worden bewaard tot uw moment van aankomst bij ons of de eventuele annulering van uw reservering</article>
-
+    <article>Uw gegevens worden bewaard tot uw moment van aankomst bij ons of de eventuele annulering van uw reservering</article><br>
+    <h2>Uw rechten</h2>
+    <article>Wij verzekeren u dat wij nooit deel zullen nemen aan gegevensmisbruik en dat uw rechten beschermd zijn</article><br>
+    <h2>Heeft u klachten?</h2>
+    <p>Bij eventuele klachten kunt u contact opnemen met: <a href="#">+06 5555 5555</a></p>
---
+++ reviews.php
@@ -0,0 +1,9 @@
+<?php
+include("header.html");?>
+<main>
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
